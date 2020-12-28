7ff04d4682fe21a40fdc89dff6b65b750cb3f19a
ChxDesk <chackop@gmail.com>
2020-12-28 16:06:39 +0000

    simple useeffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import "./App.css";
-import StarRating from "./components/StarRating";
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return <StarRating totalStars={5} />;
+  const [name, setName] = useState("initialState");
+  const [Admin, setAdmin] = useState(false);
+
+  useEffect(() => {
+    document.title = "Title" + name;
+  }, [name]);
+
+  useEffect(() => {
+    console.log(`Admin user ${Admin ? "IS admin" : "not admin"}`);
+  }, [Admin]);
+
+  return (
+    <section>
+      <p>Congos {name}</p>
+      <button onClick={() => setName("AnotherSTate")}>Change name</button>
+      <p>Admin {Admin ? "admin login" : "no admins"}</p>
+      <button onClick={() => setAdmin(true)}>set admin</button>
+    </section>
+  );
 }
 
 export default App;
