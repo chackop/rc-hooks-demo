6fe9a13a40b5df42effbd8b01b461972299b2755
ChxDesk <chackop@gmail.com>
2020-12-28 14:41:28 +0000

    simple use state

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// const [firstItem, secondPiece] = ["chax1", "chax2", "chax3"];
+// const [, , thirdPiece] = ["chax1", "chax2", "chax3"];
+// console.log("firstItem", firstItem);
+// console.log("secondPiece", secondPiece);
+// console.log("thirdPiece", thirdPiece);
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
